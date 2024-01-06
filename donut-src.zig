c51b51777e3e0d202a72aeedcdeeff982fcb079e
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2024-01-06 03:54:28 -0500

    src: precompute cos/sin values, bump to 60fps, and enthiccen the donut

--- donut-src.zig
+++ donut-src.zig
@@ -1,10 +1,10 @@
 // Based off https://www.a1k0n.net/2011/07/20/donut-math.html
 
 /// Torus minor radius (i.e. the radius of the tube)
-const R1 = 1;
+const R1 = 2;
 /// Torus major radius
 /// (i.e. the distance from the center of the tube to the center of the torus)
-const R2 = 2;
+const R2 = 3;
 
 /// Distance from camera to the viewing plane. Determines FOV.
 // Calculated based off screen size to ensure torus fills the frame
@@ -12,13 +12,13 @@ const R2 = 2;
 const K1 = screen_width * K2 * 3 / (8 * (R1 + R2));
 
 /// Distance of the donut from the camera
-const K2 = 5;
+const K2 = 15;
 
 /// Good enough approximation...
 const PI = 3.1415926;
 
-const theta_increment = 2 * PI / 89.0;
-const phi_increment = 2 * PI / 314.0;
+const theta_increment = 0.07;
+const phi_increment = 0.02;
 
 const screen_width = 50;
 const screen_height = 50;
@@ -44,23 +44,35 @@ pub fn main() !void {
         if (rot_x > 2 * PI) rot_x = 0;
         if (rot_z > 2 * PI) rot_z = 0;
     }) {
+        const cos_rot_x = @cos(rot_x);
+        const sin_rot_x = @sin(rot_x);
+
+        const cos_rot_z = @cos(rot_z);
+        const sin_rot_z = @sin(rot_z);
+
         // Compute points on a circle, rotated around the y-axis
         // to form a torus "point cloud".
         var phi: f32 = 0;
         while (phi <= 2 * PI) : (phi += phi_increment) {
+            const cos_phi = @cos(phi);
+            const sin_phi = @sin(phi);
+
             var theta: f32 = 0;
             while (theta <= 2 * PI) : (theta += theta_increment) {
+                const cos_theta = @cos(theta);
+                const sin_theta = @sin(theta);
+
                 // compute point on circle
-                const circle_x = R2 + R1 * @cos(theta);
-                const circle_y = R1 * @sin(theta);
+                const circle_x = R2 + R1 * cos_theta;
+                const circle_y = R1 * sin_theta;
                 // implicitly z = 0
 
                 // compute world-space point on torus using circle point,
                 // "sweeping" the circle around the local y-axis by phi
                 // and applying a rotation matrix.
-                const torus_x = circle_x * (@cos(rot_z) * @cos(phi) + @sin(rot_x) * @sin(rot_z) * @sin(phi)) - circle_y * @cos(rot_x) * @sin(rot_z);
-                const torus_y = circle_x * (@cos(phi) * @sin(rot_z) - @cos(rot_z) * @sin(rot_x) * @sin(phi)) + circle_y * @cos(rot_x) * @cos(rot_z);
-                const torus_z = @cos(rot_x) * circle_x * @sin(phi) + circle_y * @sin(rot_x);
+                const torus_x = circle_x * (cos_rot_z * cos_phi + sin_rot_x * sin_rot_z * sin_phi) - circle_y * cos_rot_x * sin_rot_z;
+                const torus_y = circle_x * (cos_phi * sin_rot_z - cos_rot_z * sin_rot_x * sin_phi) + circle_y * cos_rot_x * cos_rot_z;
+                const torus_z = cos_rot_x * circle_x * sin_phi + circle_y * sin_rot_x;
 
                 // computing inverse of z avoids extra divisions and makes using the z-buffer easier
                 const inverse_z = 1 / (K2 + torus_z);
@@ -69,9 +81,11 @@ pub fn main() !void {
                 const scrn_x: usize = @intFromFloat(screen_width / 2 + (K1 * inverse_z * torus_x));
                 const scrn_y: usize = @intFromFloat(screen_height / 2 - (K1 * inverse_z * torus_y)); // y is negated since in 3D +y is up, but in 2D -y is up
 
-                // a mouthful, but correct.
-                const luminance = @cos(phi) * @cos(theta) * @sin(rot_z) - @cos(rot_x) * @cos(theta) * @sin(phi) -
-                    @sin(rot_x) * @sin(theta) + @cos(rot_z) * (@cos(rot_x) * @sin(theta) - @cos(theta) * @sin(rot_x) * @sin(phi));
+                // The dot product of the surface normal and a light vector
+                // above and behind the camera (0, 1, -1). The light vector
+                // should be normalized but we handle it below. A mouthful, but correct.
+                const luminance = cos_phi * cos_theta * sin_rot_z - cos_rot_x * cos_theta * sin_phi -
+                    sin_rot_x * sin_theta + cos_rot_z * (cos_rot_x * sin_theta - cos_theta * sin_rot_x * sin_phi);
 
                 // luminance ranges from -sqrt(2) to +sqrt(2).
                 // If it's < 0 the surface is pointing away from us, so it's not worth rendering.
@@ -99,7 +113,7 @@ pub fn main() !void {
 
         @memset(&r_buffer, ' '); // clear r_buffer
         @memset(&z_buffer, 0); // clear z_buffer
-        std.time.sleep(33 * std.time.ns_per_ms);
+        std.time.sleep(16 * std.time.ns_per_ms);
     }
 
     // reset terminal
