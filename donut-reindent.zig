0ce1655ad94a8a23c7b759f6ee9bdbbac98a1150
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2024-01-11 19:47:12 -0500

    reindent: add formatted donut.zig
    Showcases the "minified" source before the shaping process.
    Easier to see the correlation between `donut-src.zig` and the
    final product.

---
+++ donut-reindent.zig
@@ -0,0 +1,67 @@
+// donut.zig
+var rb = [_]u8{32} ** 2450;
+var zb = [_]f32{0} ** 2450;
+
+pub fn main() !void {
+    const w = @import("std").io.getStdOut().writer();
+
+    try w.writeAll("\x1B[?25l");
+    try w.writeAll("\x1B[2J");
+
+    var rx: f32 = 0;
+    var rz: f32 = 0;
+    while (true) : ({
+        rx += 0.05;
+        rz += 0.03;
+    }) {
+        const crx = @cos(rx);
+        const srx = @sin(rx);
+
+        const crz = @cos(rz);
+        const srz = @sin(rz);
+
+        var p: f32 = 0;
+        while (p <= 6.283) : (p += 0.02) {
+            const cp = @cos(p);
+            const sp = @sin(p);
+
+            var t: f32 = 0;
+            while (t <= 6.283) : (t += 0.07) {
+                const ct = @cos(t);
+                const st = @sin(t);
+
+                const cx = 3 + 2 * ct;
+                const tx = cx * (crz * cp + srx * srz * sp) - (2 * st) * crx * srz;
+                const ty = cx * (srz * cp - srx * crz * sp) + (2 * st) * crx * crz;
+                const tz = 15 + crx * cx * sp + (2 * st) * srx;
+                const iz = 1 / tz;
+
+                const l = (cp * ct * srz) - (crx * ct * sp) - (srx * st) + (crz * (crx * st - ct * srx * sp));
+
+                if (l > 0) {
+                    const sx: usize = @intFromFloat(35 + (78 * iz * tx));
+                    const sy: usize = @intFromFloat(17.5 - (39 * iz * ty));
+
+                    if (iz > zb[sx + 70 * sy]) {
+                        zb[sx + 70 * sy] = iz;
+                        rb[sx + 70 * sy] = ".,-~:;=!*#$@"[@intFromFloat(l * 8)];
+                    }
+                }
+            }
+        }
+
+        try w.writeAll("\x1B[H");
+        for (0..35) |y| {
+            try w.print("{s}\n", .{rb[70 * y ..][0..70]});
+        }
+
+        @memset(&rb, 32);
+        @memset(&zb, 0);
+    }
+
+    try w.writeAll("\x1B[25h");
+} //===;::~~-.
+//--~:Based on donut.c by a1k0n~--,.
+//,-~~~Written by jdelsi~~~-,.
+//,------~~~-------,..
+//....
